1c9adf8928cf0d70dc1dfdaf080abaf8d0688d7b
Arief Taufik Rahman <arieftaufikrahman123@gmail.com>
2023-04-14 10:25:31 +0700

    Added video

---
+++ app/src/main/java/com/example/myplayer/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+    }
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this).build()
+        viewBinding.videoView.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+    }
+}
