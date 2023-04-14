7d544223c278002dacba46a5b71b1a05b025ad0a
Arief Taufik Rahman <arieftaufikrahman123@gmail.com>
2023-04-14 10:44:08 +0700

    Lifecycle

--- app/src/main/java/com/example/myplayer/MainActivity.kt
+++ app/src/main/java/com/example/myplayer/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.myplayer.databinding.ActivityMainBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +22,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
 
+        /*
         val player = ExoPlayer.Builder(this).build()
         viewBinding.videoView.player = player
-
         val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
         val anotherMediaItem = MediaItem.fromUri(URL_AUDIO)
         player.setMediaItem(mediaItem)
         player.addMediaItem(anotherMediaItem)
-        player.prepare()
+        player.prepare()*/
+    }
+
+     // Lifecycle
+    private var player: ExoPlayer? = null
+
+    private fun initializePlayer() {
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        val anotherMediaItem = MediaItem.fromUri(URL_AUDIO)
+
+        player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            viewBinding.videoView.player = exoPlayer
+            exoPlayer.setMediaItem(mediaItem)
+            exoPlayer.addMediaItem(anotherMediaItem)
+            exoPlayer.prepare()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.release()
+        player = null
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT <= 23 && player == null) {
+           initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
     }
 }
